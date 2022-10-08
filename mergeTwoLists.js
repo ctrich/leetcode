31edc02e5632271f104115793021cb9f1723ea65
ctrich <ctrich86@yahoo.com>
2022-10-08 15:41:09 -0700

    added comments

--- mergeTwoLists.js
+++ mergeTwoLists.js
@@ -11,11 +11,13 @@
  * @return {ListNode}
  */
  var mergeTwoLists = function(list1, list2) {
+    //if one list is empty return the other
     if (list1 === null) {
         return list2
     } else if (list2 === null) {
         return list1
     }
+    //get the first val to start with
     let firstVal = 0;
     if (list1.val < list2.val) {
         firstVal = list1.val;
@@ -24,8 +26,11 @@
         firstVal = list2.val;
         list2 = list2.next;
     }
+
+    //create a new list and head
     let list3 = new ListNode(Math.min(firstVal), null);
     let mergedList = list3;
+
     while(list1 !== null && list2 !== null) {
         if (list1.val < list2.val) {
             mergedList.next = list1;
@@ -36,6 +41,7 @@
         }
         mergedList = mergedList.next;
     }
+    //add the last node of whatever list that still has a node
     mergedList.next = list1 === null ? list2 : list1;
     return list3;
 };
