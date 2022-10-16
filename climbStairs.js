e9fa75d44feb65e9c44828209823d0b68c6dca24
ctrich <ctrich86@yahoo.com>
2022-10-16 16:52:42 -0700

    climbing stairs 1 or 2 steps at a time, how manu different cominations are there to get to the top of n steps

---
+++ climbStairs.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var climbStairs = function (n) {
+    let first = 1;
+    let second = 1;
+    for (let i = 0; i < n - 1; i++) {
+      let temp = first;
+      first = first + second;
+      second = temp;
+    }
+    return first;
+  };
