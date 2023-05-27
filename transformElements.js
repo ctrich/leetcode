d993ec22d1fc0b626f1a84eb8ac7520fa35ae0e5
Chris Richardson <ctrich86@yahoo.com>
2023-05-27 14:41:24 -0500

    Given an integer array and a mapping function, return a new arraywith the transormatio applied to each element

---
+++ transformElements.js
@@ -0,0 +1,13 @@
+/**
+ * Given a integer array and a mapping function, return a new array with a transformation applied to each element.
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(fn(arr[i], i));
+    }
+    return newArr;
+};
