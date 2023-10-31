84d3a197f2a70d1cf714f92e0fffa308bd0c381c
ctrich <ctrich86@yahoo.com>
2023-10-31 16:42:43 -0700

    deleted console logs

--- flatten.js
+++ flatten.js
@@ -1,13 +1,8 @@
 Array.prototype.flatten = function(arr = []) {
-    console.log(arr)
 
-    
     let result = arr
     for (let i = 0; i < this.length; i++) {
-        //console.log("in the for loop", Array.from(this[i]), i)
-        console.log(typeof this[i])
         if (typeof this[i] === "number") {
-            //console.log('t')
             result.push(this[i])
         } else {
             this[i].flatten(result)
@@ -16,6 +11,5 @@ Array.prototype.flatten = function(arr = []) {
     return result;
 }
 
-//console.log([[1, 2], [3]].flatten());
 let arr = [[1, 2], [3], [3, 4, 5, [1, 2, 3]]]
 console.log(arr.flatten());
