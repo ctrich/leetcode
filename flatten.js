16e8ef6310aba53f9a2b1d9f87320e4b9bc1e0b4
ctrich <ctrich86@yahoo.com>
2023-10-31 16:53:27 -0700

    now works for array with any data type inside instead of just numbers

--- flatten.js
+++ flatten.js
@@ -2,14 +2,14 @@ Array.prototype.flatten = function(arr = []) {
 
     let result = arr
     for (let i = 0; i < this.length; i++) {
-        if (typeof this[i] === "number") {
-            result.push(this[i])
-        } else {
+        if (typeof this[i] === "object") {
             this[i].flatten(result)
+        } else {
+            result.push(this[i])
         }
     }
     return result;
 }
 
-let arr = [[1, 2], [3], [3, 4, 5, [1, 2, 3]]]
+let arr = [[1, 'chris'], [3], [3, 4, 5, [1, 'dad', 3]]]
 console.log(arr.flatten());
